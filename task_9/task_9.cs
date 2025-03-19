a3c1519a03e28f0967bd54c6cf3bcf2add024aec
Kate777s <tainer742@gmail.com>
2025-03-19 22:24:09 +0300

    Add files via upload

---
+++ task_9/task_9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+a:
+Console.WriteLine("Введите нижний предел (a): ");
+double a;
+if (!double.TryParse(Console.ReadLine(), out a))
+{
+    Console.WriteLine("Неверно введен предел");
+    goto a;
+}
+b:
+Console.WriteLine("Введите верхний предел (b): ");
+double b;
+if (!double.TryParse(Console.ReadLine(), out b))
+{
+Console.WriteLine("Неверно введен предел");
+goto b;
+}
+n:
+Console.WriteLine("Введите кол-во разбиений (n): ");
+int n;
+if (!int.TryParse(Console.ReadLine(), out n))
+{
+    Console.WriteLine("Неверно введено разбиение");
+    goto n;
+}
+double sum = 0;
+double h = (b - a) / n;
+
+
+for (int i = 1; i < n; i++)
+{
+    double x = a + i * (h/2);
+    sum += 2 * x * x + 3 * x;
+}
+sum = h * sum;
+                
+Console.WriteLine($"Определенный интеграл от {a} до {b} примерно равен: {Math.Round(sum, 2)}");
